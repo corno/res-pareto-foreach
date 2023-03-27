7b5bbfe4da65cdae8181873791c03df51490a993
corno <corno.astn.a@schraverus.com>
2023-03-27 13:44:40 +0200

    .

--- typescript/pub/src/api.generated.ts
+++ typescript/pub/src/api.generated.ts
@@ -3,17 +3,27 @@ import * as pt from 'pareto-core-types'
 import * as g_collation from "res-pareto-collation"
 import * as g_this from "./glossary"
 
+export namespace D {
+    
+    
+    export type createDictionaryForEach = {
+        readonly 'compare': g_collation.SYNC.A.F.IsABeforeB
+    }
+    
+    export type createEnrichedDictionaryForEach = {
+        readonly 'compare': g_collation.SYNC.A.F.IsABeforeB
+    }
+    
+    
+}
+
 export namespace A {
     
     export type arrayForEach = () => g_this.SYNC.A.P.ArrayForEach
     
-    export type createDictionaryForEach = ($d: {
-        readonly 'compare': g_collation.SYNC.A.F.IsABeforeB
-    }, ) => g_this.SYNC.A.P.DictionaryForEach
+    export type createDictionaryForEach = ($d: D.createDictionaryForEach, ) => g_this.SYNC.A.P.DictionaryForEach
     
-    export type createEnrichedDictionaryForEach = ($d: {
-        readonly 'compare': g_collation.SYNC.A.F.IsABeforeB
-    }, ) => g_this.SYNC.A.P.EnrichedDictionaryForEach
+    export type createEnrichedDictionaryForEach = ($d: D.createEnrichedDictionaryForEach, ) => g_this.SYNC.A.P.EnrichedDictionaryForEach
     
     export type decorateDictionaryEntriesWithKey = () => g_this.SYNC.A.F.DecorateDictionaryEntriesWithKey
     
